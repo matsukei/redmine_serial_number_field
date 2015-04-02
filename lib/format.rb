2c108be11ff1807881f6ba3667e60a798489aad2
maeda-m <maeda-m@matsukei.co.jp>
2015-04-02 17:48:26 +0900

    Add comment

--- lib/format.rb
+++ lib/format.rb
@@ -55,23 +55,35 @@ module SerialNumberField
         matcher = generate_matcher(custom_field, datetime)
         custom_values = custom_field.custom_values.where(
           customized_id: issue.project.issues.map(&:id)).map(&:value)
-
+        # custom_values #=> e.g. ['2014-001', '2014-002', '14-0001', ...]
         custom_values.select { |value| value =~ matcher }.sort.last
       end
 
       def replace_format_value(custom_field)
         if block_given?
           custom_field.regexp.gsub(MATCHERS[:FORMAT_WRAPPER]) do |format_value_with_brace|
+            # format_value_with_brace #=> e.g. '{yy}', '{0000}'
+            # $1.clone #=> e.g. 'yy', '0000'
             yield($1.clone)
           end
         end
       end
 
+      # e.g. /2015-\d{3}/
       def generate_matcher(custom_field, datetime)
         matcher_str = replace_format_value(custom_field) do |format_value|
           if date_format_keys.include?(format_value)
             generate_date_value(format_value, datetime)
           else
+            # TODO:
+            # 
+            # CustomField of A.regexp #=> '{yy}-0{000}'
+            #   matcher of A: /15-0\d{3}/
+            # CustomField of B.regexp #=> '{yy}-{0000}'
+            #   matcher of B: /15-\d{4}/
+            #
+            # if CustomValue is '15-0999',
+            # CustomValue matches matcher of both A and B
             generate_number_matcher(format_value)
           end
         end
@@ -79,6 +91,7 @@ module SerialNumberField
         Regexp.new(matcher_str)
       end
 
+      # e.g. '\d{3}'
       def generate_number_matcher(format_value)
         ['\d', '{', format_value.size.to_s, '}'].join
       end
@@ -93,9 +106,11 @@ module SerialNumberField
         end
       end
 
+      # e.g. '2015'
       def generate_date_value(format_value, datetime)
         parse_conf = DATE_FORMATS[format_value.to_sym]
         if parse_conf.key?(:financial_year) && parse_conf[:financial_year]
+          # TODO initializers
           DateTime.fiscal_zone = :japan
           datetime = datetime.beginning_of_financial_year
         end
@@ -103,6 +118,7 @@ module SerialNumberField
         datetime.strftime(parse_conf[:strftime])
       end
 
+      # e.g. '0001'
       def generate_number_value(format_value)
         '1'.rjust(format_value.size, '0')
       end
