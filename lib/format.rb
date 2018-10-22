f7b3dbe8c7445d7b9517b691b9bfe1a74f137ad6
rambowm <rambow_m@ukw.de>
2018-10-22 14:30:26 +0200

    added ISO format target (eg. 20181022)

--- lib/format.rb
+++ lib/format.rb
@@ -10,6 +10,7 @@ module SerialNumberField
     MATCHERS = {
       :TOW_DIGIT_NUMBER => /\d{2}/,
       :FOUR_DIGIT_NUMBER => /\d{4}/,
+      :THREE_DIGIT_NUMBER => /\d{3}/,
       :FORMAT_WRAPPER => /\{(.+?)\}/
     }
 
@@ -17,7 +18,8 @@ module SerialNumberField
       :'yyyy' => { :strftime => '%Y', :financial_year => false, :regexp => MATCHERS[:FOUR_DIGIT_NUMBER] },
       :'yy'   => { :strftime => '%y', :financial_year => false, :regexp => MATCHERS[:TOW_DIGIT_NUMBER] },
       :'YYYY' => { :strftime => '%Y', :financial_year => true, :regexp => MATCHERS[:FOUR_DIGIT_NUMBER] },
-      :'YY'   => { :strftime => '%y', :financial_year => true, :regexp => MATCHERS[:TOW_DIGIT_NUMBER] }
+      :'YY'   => { :strftime => '%y', :financial_year => true, :regexp => MATCHERS[:TOW_DIGIT_NUMBER] },
+      :'ISO'   => { :strftime => '%Y%m%d', :financial_year => false, :regexp => MATCHERS[:THREE_DIGIT_NUMBER] }
     }
 
     def validate_custom_field(custom_field)
